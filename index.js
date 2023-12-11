dac65e424f66e16a83f6c04f2bb7fe2a61b34704
Andres <andresitojg96@gmail.com>
2023-12-11 15:57:26 -0500

    correcccion jason

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-
-// server.js
-const express = require('express');
-const app = express();
-const port = 3000;
-
-// Definir una ruta de ejemplo
-app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
-});
-
-// Escuchar en el puerto especificado
-app.listen(port, () => {
-  console.log(`Server 3000 activated`);
-});
+console.log("hello world")
+import express from 'express'
