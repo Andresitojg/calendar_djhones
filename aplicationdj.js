c7fd7b1dabeddceb9ba230feded8e0d71662051d
andres guerrero <andresitojg96@gmail.com>
2024-02-28 12:39:57 -0500

    2802

--- aplicationdj.js
+++ aplicationdj.js
@@ -1,20 +1,6 @@
 
 $(function(){
-    /*
-    $('#saludo').css({"color":"red"})
-    .css({"maxwith":"100px"})
     
-    $("#btn-hide").click(function(){
-    
-    $("#saludo").slideUp();
-    
-    })
-    $("#btn-show").click(function(){
-           $("#saludo").slideDown();
-        
-        })
-       
-    */
     
     $("#btn-step1").click(function(){
     
@@ -29,11 +15,27 @@ $(function(){
     
     
     })
+    $("#btn2-step2").click(function(){
+    
+        
+        $("#step2").hide()
+       $("#step3").show()
+   
+   
+   })
+   $("#btn3-step2").click(function(){
+    
+        
+    $("#step2").hide()
+   $("#step3").show()
+
+
+})
     $("#btn-step3").click(function(){
     
         
         $("#step3").hide()
-       $("#step5A").show()
+       $("#step4").show()
     
     
     })
@@ -45,23 +47,31 @@ $(function(){
     
     
     })
-    $("#btn-step4A").click(function(){
+    $("#btn3-step3").click(function(){
     
         
-        $("#step4A").hide()
-       $("#step5B").show()
-    
-    
-    })
-    $("#btn2-step4A").click(function(){
-    
-        
-        $("#step4A").hide()
+        $("#step3").hide()
        $("#step4").show()
     
     
     })
-    $("#btn2-step2").click(function(){
+    $("#btn-step4").click(function(){
+    
+        
+        $("#step4").hide()
+       $("#steporo").show()
+    
+    
+    })
+    $("#btn2-step4").click(function(){
+    
+        
+        $("#step4").hide()
+       $("#step5").show()
+    
+    
+    })
+    $("#btn3-step4").click(function(){
     
         
         $("#step2").hide()
@@ -69,62 +79,5 @@ $(function(){
     
     
     })
-    
-    
-    
-    
-    })
-    $("#btn-actcountdown").click(function(){
-        $("#btn-actcountdown").hide();
-        iniciarcuentaregresiva();
-       
         
-    })
-    
-    
-    
-     
-    
-    
-    function iniciarcuentaregresiva(){
-        var numero = 10;
-        var elementocontador =$("#countdown");
-        var ahora = new Date ();
-        var hora = ahora.getHours();
-      var intervalo = setInterval(function(){
-        elementocontador.text(numero);
-        numero--;
-        
-        if (numero < 0){
-            clearInterval(intervalo);
-            elementocontador.text('listo');
-            window.location.href = "tel:+573042067340";
-        }
-        if (hora < 8 || hora >= 18 ){
-            $("#asesor").text('Nuestra Clínica está Cerrada');
-            $("#asesorh2").text(' Abrimos a las 8 am, nuestra linea de Atención es (+57) 304 206 7340 ')
-        } else {
-            $("#asesor").text('Clínica Abierta');
-            $("#asesorh2").text('Te estamos conectando con nuestra Área de contact center')
-        }
-    }, 1000);
-     /*
-        var numero=3;
-        var elementocontador =$("#countdown");
-        var ahora = new date ();
-        var hora = ahora.gethours();
-      var intervalo = setInterval(function(){
-            elementocontador.text(numero);
-            numero--;
-            if (numero < 0){
-                clearInterval(intervalo);
-                elementocontador.text('listo');
-            }
-            if (hora < 8 || hora > 18){
-                $("#asesor").text('Clínica Cerrada');
-            } else {
-                $("#asesor").text('Clínica Abierta');
-            }
-        }, 1000);  // "setInterval" instead of "setinterval", and added milliseconds (1000) for 1 second interval
-    */
-    }
+})
