001b37657ab1738cad7340c31019f3197659ef3e
andres <andresitojg96@gmail.com>
2024-06-27 16:58:39 -0500

    status

---
+++ etapas.js
@@ -0,0 +1,107 @@
+
+$(function(){
+    
+    
+    $("#btn-step1").click(function(){
+    
+        $("#step1").hide();
+        $("#step2").show();
+    });
+    $("#btn-step2").click(function(){
+    
+        
+         $("#step2").hide()
+        $("#step3").show()
+    
+    
+    })
+    $("#btn2-step2").click(function(){
+    
+        
+        $("#step2").hide()
+       $("#step3").show()
+   
+   
+   })
+   $("#btn3-step2").click(function(){
+    
+        
+    $("#step2").hide()
+   $("#step3").show()
+
+
+})
+    $("#btn-step3").click(function(){
+    
+        
+        $("#step3").hide()
+       $("#step4").show()
+    
+    
+    })
+    $("#btn2-step3").click(function(){
+    
+        
+        $("#step3").hide()
+       $("#step4").show()
+    
+    
+    })
+    $("#btn3-step3").click(function(){
+    
+        
+        $("#step3").hide()
+       $("#step6").show()
+    
+    
+    })
+    $("#btn-step4").click(function(){
+    
+        
+        $("#step4").hide()
+       $("#steporo").show()
+    
+    
+    })
+    $("#btn2-step4").click(function(){
+    
+        
+        $("#step4").hide()
+       $("#stepplatinum").show()
+    
+    
+    })
+    $("#btn3-step4").click(function(){
+    
+        
+        $("#step4").hide()
+       $("#stepplatinum").show()
+    
+    
+    })
+    $("#btn1-step6").click(function(){
+    
+        
+        $("#step6").hide()
+       $("#step7").show()
+    
+    
+    })
+    $("#btn1-step7").click(function(){
+    
+        
+        $("#step7").hide()
+       $("#step8").show()
+    
+    
+    })
+    $("#btn4-plt").click(function(){
+    
+        
+        $("#stepplatinum").hide()
+       $("#step6").show()
+    
+    
+    })
+        
+})
